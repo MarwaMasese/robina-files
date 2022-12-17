72873c9fa97d1fc50bda165275c92e46a184cae2
SentiCryptdev <senticrypt@gmail.com>
2022-12-17 08:48:17 +0300

    Robina's project

---
+++ home.js
@@ -0,0 +1,23 @@
+// This is the home page
+const Home = () => {
+    return ( 
+        <body class="home">
+    
+            <h1 class="title"> Flock together</h1>
+            
+            <div class ="row">
+                <div class = "column">
+                    <button class="login"> Login</button>
+                </div>
+                <div class = "column">
+                    <button class="signup"> Sign Up </button>
+                </div>
+            </div>
+        </body>
+     );
+}
+ 
+export default Home;
+
+
+
